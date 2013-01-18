1fd7fc79455bd71bc6ca9fe5f92f7ebcbd2ee182
Stuart Slattery <sslattery617@gmail.com>
2013-01-18 14:02:53 -0600

    adding eetra adjoint domain test

---
+++ epetra/test/tstEpetraAdjointDomain.cpp
@@ -0,0 +1,438 @@
+//---------------------------------------------------------------------------//
+/*!
+ * \file tstEpetraAdjointDomain.cpp
+ * \author Stuart R. Slattery
+ * \brief Epetra AdjointDomain tests.
+ */
+//---------------------------------------------------------------------------//
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <sstream>
+#include <algorithm>
+#include <string>
+#include <cassert>
+
+#include <MCLS_AdjointDomain.hpp>
+#include <MCLS_VectorTraits.hpp>
+#include <MCLS_EpetraAdapter.hpp>
+#include <MCLS_History.hpp>
+#include <MCLS_AdjointTally.hpp>
+#include <MCLS_Events.hpp>
+#include <MCLS_RNGControl.hpp>
+
+#include <Teuchos_UnitTestHarness.hpp>
+#include <Teuchos_DefaultComm.hpp>
+#include <Teuchos_CommHelpers.hpp>
+#include <Teuchos_RCP.hpp>
+#include <Teuchos_ArrayRCP.hpp>
+#include <Teuchos_Array.hpp>
+#include <Teuchos_ArrayView.hpp>
+#include <Teuchos_TypeTraits.hpp>
+#include <Teuchos_ParameterList.hpp>
+
+#include <Epetra_Map.h>
+#include <Epetra_Vector.h>
+#include <Epetra_RowMatrix.h>
+#include <Epetra_CrsMatrix.h>
+#include <Epetra_Comm.h>
+#include <Epetra_SerialComm.h>
+#include <Epetra_MpiComm.h>
+
+//---------------------------------------------------------------------------//
+// Helper functions.
+//---------------------------------------------------------------------------//
+
+Teuchos::RCP<Epetra_Comm> getEpetraComm( 
+    const Teuchos::RCP<const Teuchos::Comm<int> >& comm )
+{
+#ifdef HAVE_MPI
+    Teuchos::RCP< const Teuchos::MpiComm<int> > mpi_comm = 
+	Teuchos::rcp_dynamic_cast< const Teuchos::MpiComm<int> >( comm );
+    Teuchos::RCP< const Teuchos::OpaqueWrapper<MPI_Comm> > opaque_comm = 
+	mpi_comm->getRawMpiComm();
+    return Teuchos::rcp( new Epetra_MpiComm( (*opaque_comm)() ) );
+#else
+    return Teuchos::rcp( new Epetra_SerialComm() );
+#endif
+}
+
+//---------------------------------------------------------------------------//
+// Test templates
+//---------------------------------------------------------------------------//
+TEUCHOS_UNIT_TEST( AdjointDomain, Typedefs )
+{
+    typedef Epetra_Vector VectorType;
+    typedef MCLS::VectorTraits<VectorType> VT;
+    typedef Epetra_RowMatrix MatrixType;
+    typedef MCLS::MatrixTraits<VectorType,MatrixType> MT;
+    typedef MCLS::AdjointDomain<VectorType,MatrixType> DomainType;
+    typedef MCLS::History<double,int> HistoryType;
+    typedef MCLS::AdjointTally<VectorType> TallyType;
+    typedef typename DomainType::HistoryType history_type;
+    typedef typename DomainType::TallyType tally_type;
+
+    TEST_EQUALITY_CONST( 
+	(Teuchos::TypeTraits::is_same<HistoryType, history_type>::value)
+	== true, true );
+    TEST_EQUALITY_CONST( 
+	(Teuchos::TypeTraits::is_same<TallyType, tally_type>::value)
+	== true, true );
+}
+
+//---------------------------------------------------------------------------//
+TEUCHOS_UNIT_TEST( AdjointDomain, NoOverlap )
+{
+    typedef Epetra_Vector VectorType;
+    typedef MCLS::VectorTraits<VectorType> VT;
+    typedef Epetra_RowMatrix MatrixType;
+    typedef MCLS::MatrixTraits<VectorType,MatrixType> MT;
+    typedef MCLS::History<double,int> HistoryType;
+    typedef MCLS::AdjointTally<VectorType> TallyType;
+
+    int local_num_rows = 10;
+    int global_num_rows = local_num_rows*comm_size;
+    Teuchos::RCP<Epetra_Map> map = Teuchos::rcp(
+	new Epetra_Map( global_num_rows, 0, *epetra_comm ) );
+
+    // Build the linear operator and solution vector.
+    Teuchos::RCP<Epetra_CrsMatrix> A = 	
+	Teuchos::rcp( new Epetra_CrsMatrix( Copy, *map, 0 ) );
+
+    Teuchos::Array<int> global_columns( 2 );
+    Teuchos::Array<double> values( 2 );
+    for ( int i = 1; i < global_num_rows; ++i )
+    {
+	global_columns[0] = i-1;
+	global_columns[1] = i;
+	values[0] = 2;
+	values[1] = 3;
+	A->InsertGlobalValues( i, global_columns().size(), 
+			       &values[0], &global_columns[0] );
+    }
+    A->FillComplete();
+
+    Teuchos::RCP<MatrixType> B = A;
+    Teuchos::RCP<VectorType> x = MT::cloneVectorFromMatrixRows( *B );
+
+    // Build the adjoint domain.
+    Teuchos::ParameterList plist;
+    plist.set<int>( "Overlap Size", 0 );
+    MCLS::AdjointDomain<VectorType,MatrixType> domain( B, x, plist );
+
+    // Check the tally.
+    double x_val = 2;
+    Teuchos::RCP<TallyType> tally = domain.domainTally();
+    for ( int i = 0; i < global_num_rows; ++i )
+    {
+	if ( i >= local_num_rows*comm_rank && i < local_num_rows*(comm_rank+1) )
+	{
+	    HistoryType history( i, x_val );
+	    history.live();
+	    tally->tallyHistory( history );
+	}
+    }
+
+    tally->combineTallies();
+
+    Teuchos::ArrayRCP<const double> x_view = VT::view( *x );
+    typename Teuchos::ArrayRCP<const double>::const_iterator x_view_iterator;
+    for ( x_view_iterator = x_view.begin();
+	  x_view_iterator != x_view.end();
+	  ++x_view_iterator )
+    {
+	TEST_EQUALITY( *x_view_iterator, x_val );
+    }
+
+    // Check the boundary.
+    if ( comm_rank == comm_size-1 )
+    {
+	TEST_EQUALITY( domain.numNeighbors(), 0 );
+    }
+    else
+    {
+	TEST_EQUALITY( domain.numNeighbors(), 1 );
+	TEST_EQUALITY( domain.neighborRank(0), comm_rank+1 );
+	TEST_EQUALITY( domain.owningNeighbor(local_num_rows*(comm_rank+1)), 0 );
+    }
+
+    for ( int i = 0; i < global_num_rows; ++i )
+    {
+	if ( i >= local_num_rows*comm_rank && i < local_num_rows*(comm_rank+1) )
+	{
+	    TEST_ASSERT( domain.isLocalState(i) );
+	}
+	else
+	{
+	    TEST_ASSERT( !domain.isLocalState(i) );
+	}
+    }
+}
+
+//---------------------------------------------------------------------------//
+TEUCHOS_UNIT_TEST( AdjointDomain, SomeOverlap )
+{
+    typedef Epetra_Vector VectorType;
+    typedef MCLS::VectorTraits<VectorType> VT;
+    typedef Epetra_RowMatrix MatrixType;
+    typedef MCLS::MatrixTraits<VectorType,MatrixType> MT;
+    typedef MCLS::History<double,int> HistoryType;
+    typedef MCLS::AdjointTally<VectorType> TallyType;
+
+    int local_num_rows = 10;
+    int global_num_rows = local_num_rows*comm_size;
+    Teuchos::RCP<Epetra_Map> map = Teuchos::rcp(
+	new Epetra_Map( global_num_rows, 0, *epetra_comm ) );
+
+    // Build the linear operator and solution vector.
+    Teuchos::RCP<Epetra_CrsMatrix> A = 	
+	Teuchos::rcp( new Epetra_CrsMatrix( Copy, *map, 0 ) );
+    Teuchos::Array<int> global_columns( 2 );
+    Teuchos::Array<double> values( 2 );
+    for ( int i = 1; i < global_num_rows; ++i )
+    {
+	global_columns[0] = i-1;
+	global_columns[1] = i;
+	values[0] = 2;
+	values[1] = 3;
+	A->InsertGlobalValues( i, global_columns().size(), 
+			       &values[0], &global_columns[0] );
+    }
+    A->FillComplete();
+
+    Teuchos::RCP<MatrixType> B = A;
+    Teuchos::RCP<VectorType> x = MT::cloneVectorFromMatrixRows( *B );
+
+    // Build the adjoint domain.
+    Teuchos::ParameterList plist;
+    plist.set<int>( "Overlap Size", 2 );
+    MCLS::AdjointDomain<VectorType,MatrixType> domain( B, x, plist );
+
+    // Check the tally.
+    double x_val = 2;
+    Teuchos::RCP<TallyType> tally = domain.domainTally();
+    for ( int i = 0; i < global_num_rows; ++i )
+    {
+	if ( i >= local_num_rows*comm_rank && i < 2+local_num_rows*(comm_rank+1) )
+	{
+	    HistoryType history( i, x_val );
+	    history.live();
+	    tally->tallyHistory( history );
+	}
+    }
+
+    tally->combineTallies();
+
+    Teuchos::ArrayRCP<const double> x_view = VT::view( *x );
+    for ( int i = 0; i < local_num_rows; ++i )
+    {
+	if ( comm_rank == 0 || i > 1 )
+	{
+	    TEST_EQUALITY( x_view[i], x_val );
+	}
+	else
+	{
+	    TEST_EQUALITY( x_view[i], 2*x_val );
+	}
+    }
+
+    // Check the boundary.
+    if ( comm_rank == comm_size-1 )
+    {
+	TEST_EQUALITY( domain.numNeighbors(), 0 );
+    }
+    else
+    {
+	TEST_EQUALITY( domain.numNeighbors(), 1 );
+	TEST_EQUALITY( domain.neighborRank(0), comm_rank+1 );
+	TEST_EQUALITY( domain.owningNeighbor(2+local_num_rows*(comm_rank+1)), 0 );
+    }
+
+    if ( comm_rank == comm_size-1 )
+    {
+	for ( int i = 0; i < global_num_rows; ++i )
+	{
+	    if ( i >= local_num_rows*comm_rank && i < local_num_rows*(comm_rank+1) )
+	    {
+		TEST_ASSERT( domain.isLocalState(i) );
+	    }
+	    else
+	    {
+		TEST_ASSERT( !domain.isLocalState(i) );
+	    }
+	}
+    }
+    else
+    {
+	for ( int i = 0; i < global_num_rows; ++i )
+	{
+	    if ( i >= local_num_rows*comm_rank && i < 2+local_num_rows*(comm_rank+1) )
+	    {
+		TEST_ASSERT( domain.isLocalState(i) );
+	    }
+	    else
+	    {
+		TEST_ASSERT( !domain.isLocalState(i) );
+	    }
+	}
+    }
+}
+
+//---------------------------------------------------------------------------//
+TEUCHOS_UNIT_TEST( AdjointDomain, Transition )
+{
+    typedef Epetra_Vector VectorType;
+    typedef MCLS::VectorTraits<VectorType> VT;
+    typedef Epetra_RowMatrix MatrixType;
+    typedef MCLS::MatrixTraits<VectorType,MatrixType> MT;
+    typedef MCLS::History<double,int> HistoryType;
+    typedef MCLS::AdjointTally<VectorType> TallyType;
+
+    int local_num_rows = 10;
+    int global_num_rows = local_num_rows*comm_size;
+    Teuchos::RCP<Epetra_Map> map = Teuchos::rcp(
+	new Epetra_Map( global_num_rows, 0, *epetra_comm ) );
+
+    // Build the linear operator and solution vector.
+    Teuchos::RCP<Epetra_CrsMatrix> A = 	
+	Teuchos::rcp( new Epetra_CrsMatrix( Copy, *map, 0 ) );
+    Teuchos::Array<int> global_columns( 2 );
+    Teuchos::Array<double> values( 2 );
+    for ( int i = 1; i < global_num_rows; ++i )
+    {
+	global_columns[0] = i-1;
+	values[0] = -0.5;
+	A->InsertGlobalValues( i, global_columns().size(), 
+			       &values[0], &global_columns[0] );
+    }
+    global_columns[0] = global_num_rows-1;
+    values[0] = -0.5;
+    A->InsertGlobalValues( global_num_rows-1, global_columns.size(), 
+			   &values[0], &global_columns[0] );
+    A->FillComplete();
+
+    Teuchos::RCP<MatrixType> B = A;
+    Teuchos::RCP<VectorType> x = MT::cloneVectorFromMatrixRows( *B );
+
+    // Build the adjoint domain.
+    Teuchos::ParameterList plist;
+    plist.set<int>( "Overlap Size", 2 );
+    MCLS::AdjointDomain<VectorType,MatrixType> domain( B, x, plist );
+
+    // Process a history transition in the domain.
+    MCLS::RNGControl control( 2394723 );
+    MCLS::RNGControl::RNG rng = control.rng( 4 );
+    double weight = 3.0; 
+    for ( int i = 0; i < global_num_rows-1; ++i )
+    {
+	if ( comm_rank == comm_size - 1 )
+	{
+	    if ( i >= local_num_rows*comm_rank && i < local_num_rows*(comm_rank+1) )
+	    {
+		HistoryType history( i, weight );
+		history.live();
+		history.setEvent( MCLS::TRANSITION );
+		history.setRNG( rng );
+		domain.processTransition( history );
+
+		TEST_EQUALITY( history.state(), i+1 );
+		TEST_EQUALITY( history.weight(), weight*comm_size / 2 );
+	    }
+	}
+	else
+	{
+	    if ( i >= local_num_rows*comm_rank && i < 2+local_num_rows*(comm_rank+1) )
+	    {
+		HistoryType history( i, weight );
+		history.live();
+		history.setEvent( MCLS::TRANSITION );
+		history.setRNG( rng );
+		domain.processTransition( history );
+
+		TEST_EQUALITY( history.state(), i+1 );
+		TEST_EQUALITY( history.weight(), weight*comm_size / 2 );
+	    }
+	}
+    }
+}
+
+//---------------------------------------------------------------------------//
+TEUCHOS_UNIT_TEST( AdjointDomain, Diagonal )
+{
+    typedef Epetra_Vector VectorType; 
+    typedef MCLS::VectorTraits<VectorType> VT;
+    typedef Epetra_RowMatrix MatrixType;
+    typedef MCLS::MatrixTraits<VectorType,MatrixType> MT;
+    typedef MCLS::History<double,int> HistoryType;
+    typedef MCLS::AdjointTally<VectorType> TallyType;
+
+    int local_num_rows = 10;
+    int global_num_rows = local_num_rows*comm_size;
+    Teuchos::RCP<Epetra_Map> map = Teuchos::rcp(
+	new Epetra_Map( global_num_rows, 0, *epetra_comm ) );
+
+    // Build the linear operator and solution vector.
+    Teuchos::RCP<Epetra_CrsMatrix> A = 	
+	Teuchos::rcp( new Epetra_CrsMatrix( Copy, *map, 0 ) );
+    Teuchos::Array<int> global_columns( 2 );
+    Teuchos::Array<double> values( 2 );
+    for ( int i = 0; i < global_num_rows; ++i )
+    {
+	global_columns[0] = i;
+	values[0] = 3.0;
+	A->InsertGlobalValues( i, global_columns().size(), 
+			       &values[0], &global_columns[0] );
+    }
+    A->FillComplete();
+
+    Teuchos::RCP<MatrixType> B = A;
+    Teuchos::RCP<VectorType> x = MT::cloneVectorFromMatrixRows( *B );
+
+    // Build the adjoint domain.
+    Teuchos::ParameterList plist;
+    plist.set<int>( "Overlap Size", 2 );
+    MCLS::AdjointDomain<VectorType,MatrixType> domain( B, x, plist );
+
+    // Process a history transition in the domain.
+    MCLS::RNGControl control( 2394723 );
+    MCLS::RNGControl::RNG rng = control.rng( 4 );
+    double weight = 3.0; 
+    for ( int i = 0; i < global_num_rows; ++i )
+    {
+	if ( comm_rank == comm_size - 1 )
+	{
+	    if ( i >= local_num_rows*comm_rank && i < local_num_rows*(comm_rank+1) )
+	    {
+		HistoryType history( i, weight );
+		history.live();
+		history.setEvent( MCLS::TRANSITION );
+		history.setRNG( rng );
+		domain.processTransition( history );
+
+		TEST_EQUALITY( history.state(), i );
+		TEST_EQUALITY( history.weight(), weight*(comm_size*3-1) );
+	    }
+	}
+	else
+	{
+	    if ( i >= local_num_rows*comm_rank && i < 2+local_num_rows*(comm_rank+1) )
+	    {
+		HistoryType history( i, weight );
+		history.live();
+		history.setEvent( MCLS::TRANSITION );
+		history.setRNG( rng );
+		domain.processTransition( history );
+
+		TEST_EQUALITY( history.state(), i );
+		TEST_EQUALITY( history.weight(), weight*(comm_size*3-1) );
+	    }
+	}
+    }
+}
+
+//---------------------------------------------------------------------------//
+// end tstEpetraAdjointDomain.cpp
+//---------------------------------------------------------------------------//
+
