1f8e987dc71fa927a50624c5a13c86bfc59fc3a9
Stuart Slattery <sslattery617@gmail.com>
2013-01-25 16:07:31 -0600

    adding adjoint solver

--- src/MCLS_AdjointSolver.hpp
+++ src/MCLS_AdjointSolver.hpp
@@ -32,40 +32,82 @@
 */
 //---------------------------------------------------------------------------//
 /*!
- * \file MCLS_Solver.hpp
+ * \file MCLS_Adjoint Solver.hpp
  * \author Stuart R. Slattery
- * \brief Linear solver base class.
+ * \brief Adjoint Monte Carlo solver declaration.l
  */
 //---------------------------------------------------------------------------//
 
-#ifndef MCLS_SOLVER_HPP
-#define MCLS_SOLVER_HPP
+#ifndef MCLS_ADJOINTSOLVER_HPP
+#define MCLS_ADJOINTSOLVER_HPP
+
+#include "MCLS_Solver.hpp"
+#include "MCLS_LinearProblem.hpp"
+#include "MCLS_VectorTraits.hpp"
+#include "MCLS_MatrixTraits.hpp"
+#include "MCLS_AdjointDomain.hpp"
+#include "MCLS_SourceTransporter.hpp"
+
+#include <Teuchos_RCP.hpp>
+#include <Teuchos_Comm.hpp>
+#include <Teuchos_ParameterList.hpp>
 
 namespace MCLS
 {
 
 //---------------------------------------------------------------------------//
 /*!
- * \class Solver
+ * \class Adjointsolver
  * \brief Linear solver base class.
  */
-class Solver
+template<class Vector, class Matrix>
+class AdjointSolver : public Solver
 {
   public:
 
+    //@{
+    //! Typedefs.
+    typedef Vector                                      vector_type;
+    typedef Matrix                                      matrix_type;
+    typedef VectorTraits<Vector>                        VT;
+    typedef MatrixTraits<Vector,Matrix>                 MT;
+    typedef LinearProblem<Vector,Matrix>                LinearProblemType;
+    typedef AdjointDomain<Vector,Matrix>                DomainType;
+    typedef SourceTransporter<DomainType>               TransporterType;
+    typedef typename TransporterType::SourceType        SourceType;
+    typedef Teuchos::Comm<int>                          Comm;
+    //@}
+
+    // Constructor.
+    AdjointSolver( const Teuchos::RCP<LinearProblemType>& linear_problem,
+		   const Teuchos::RCP<const Comm>& global_comm,
+		   Teuchos::ParameterList& plist );
+
     //! Destructor.
-    virtual ~Solver { /* ... */ }
+    ~AdjointSolver { /* ... */ }
 
-    //! Solve the linear problem.
-    virtual void solve() = 0;
+    // Solve the linear problem.
+    void solve();
 
-    //! Return whether the solution has converged.
-    virtual bool isConverged() = 0;
+    // Return whether the solution has converged.
+    bool isConverged();
 
   private:
 
-    //! Private constructor. This forces construction through the factory.
-    Solver() { /* ... */ }
+    // Linear problem.
+    Teuchos::RCP<LinearProblemType> d_linear_problem;
+
+    // Global problem communicator.
+    Teuchos::RCP<const Comm> d_global_comm;
+
+    // Local domain.
+    Teuchos::RCP<DomainType> d_domain;
+
+    // Source transporter.
+    Teuchos::RCP<TransporterType> d_transporter;
+
+    // Source.
+    Teuchos::RCP<SourceType> d_source;
 };
 
 //---------------------------------------------------------------------------//
@@ -73,10 +115,16 @@ class Solver
 } // end namespace MCLS
 
 //---------------------------------------------------------------------------//
+// Template includes.
+//---------------------------------------------------------------------------//
 
-#endif // end MCLS_SOLVER_HPP
+#include "MCLS_AdjointSolver_impl.hpp"
 
 //---------------------------------------------------------------------------//
-// end MCLS_Solver.hpp
+
+#endif // end MCLS_ADJOINTSOLVER_HPP
+
+//---------------------------------------------------------------------------//
+// end MCLS_Adjointsolver.hpp
 // ---------------------------------------------------------------------------//
 
