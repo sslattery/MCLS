1f370950fc075cf91d7288b69822c3ffe22365f0
Stuart Slattery <sslattery617@gmail.com>
2013-01-28 11:03:01 -0600

    adding adjoint monte carlo solver manager

---
+++ src/MCLS_AdjointSolverManager.hpp
@@ -0,0 +1,126 @@
+//---------------------------------------------------------------------------//
+/*
+  Copyright (c) 2012, Stuart R. Slattery
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+
+  *: Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+
+  *: Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
+
+  *: Neither the name of the University of Wisconsin - Madison nor the
+  names of its contributors may be used to endorse or promote products
+  derived from this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+//---------------------------------------------------------------------------//
+/*!
+ * \file MCLS_AdjointSolverManager.hpp
+ * \author Stuart R. Slattery
+ * \brief Linear solver manager base class.
+ */
+//---------------------------------------------------------------------------//
+
+#ifndef MCLS_ADJOINTSOLVERMANAGER_HPP
+#define MCLS_ADJOINTSOLVERMANAGER_HPP
+
+#include <MCLS_LinearProblem.hpp>
+#include <MCLS_VectorTraits.hpp>
+
+#include <Teuchos_RCP.hpp>
+#include <Teuchos_Describable.hpp>
+#include <Teuchos_ParameterList.hpp>
+#include <Teuchos_ScalarTraits.hpp>
+
+namespace MCLS
+{
+
+//---------------------------------------------------------------------------//
+/*!
+ * \class AdjointSolverManager
+ * \brief Linear solver base class.
+ */
+template<class Vector, class Matrix>
+class AdjointSolverManager : public Teuchos::Describable
+{
+  public:
+
+    //@{
+    //! Typedefs.
+    typedef Vector                                  vector_type;
+    typedef VectorTraits<Vector>                    VT;
+    typedef typename VT::scalar_type                Scalar;
+    typedef Matrix                                  matrix_type;
+    //@}
+
+    //! Constructor.
+    AdjointSolverManager() { /* ... */ }
+
+    //! Destructor.
+    virtual ~AdjointSolverManager { /* ... */ }
+
+    //! Get the linear problem being solved by the manager.
+    virtual const LinearProblem<Vector,Matrix>& getProblem() const = 0;
+
+    //! Get the valid parameters for this manager.
+    virtual Teuchos::RCP<const Teuchos::ParameterList> 
+    getValidParameters() const = 0;
+
+    //! Get the current parameters being used for this manager.
+    virtual Teuchos::RCP<const Teuchos::ParameterList> 
+    getCurrentParameters() const = 0;
+
+    //! Get the tolerance achieved on the last linear solve. This may be less
+    //! or more than the set convergence tolerance.
+    virtual Teuchos::ScalarTraits<Scalar>::magnitudeType 
+    achievedTol() const = 0;
+
+    //! Get the number of iterations from the last linear solve.
+    virtual in getNumIters() const = 0;
+
+    //! Set the linear problem with the manager.
+    virtual void setProblem( 
+	const Teuchos::RCP<LinearProblem<Vector,Matrix> >& problem ) = 0;
+
+    //! Set the parameters for the manager. The manager will modify this list
+    //! with default parameters that are not defined.
+    virtual void setParameters( 
+	const Teuchos::RCP<Teuchos::ParameterList>& params ) = 0;
+
+    //! Solve the linear problem. Return true if the solution converged. False
+    //! if it did not.
+    virtual bool solve() = 0;
+
+    //! Return if the last linear solve converged.
+    virtual bool getConvergedStatus() const = 0;
+};
+
+//---------------------------------------------------------------------------//
+
+} // end namespace MCLS
+
+//---------------------------------------------------------------------------//
+
+#endif // end MCLS_ADJOINTSOLVERMANAGER_HPP
+
+//---------------------------------------------------------------------------//
+// end MCLS_AdjointSolverManager.hpp
+// ---------------------------------------------------------------------------//
+
