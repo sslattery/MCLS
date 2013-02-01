c2b0dbc39f9e086ebb71d2e608c2425c1a217765
Stuart Slattery <sslattery617@gmail.com>
2013-02-01 12:18:53 -0600

    finished delcaration file for Thyra::LinearOpWithSolve

--- thyra/src/Thyra_MCLSLinearOpWithSolve.hpp
+++ thyra/src/Thyra_MCLSLinearOpWithSolve.hpp
@@ -42,11 +42,11 @@
 #define THYRA_MCLS_LINEAR_OP_WITH_SOLVE_HPP
 
 #include "MCLS_SolverManager.hpp"
+#include "Thyra_MCLSLinearProblemAdapter.hpp"
+#include "Thyra_MCLSSolverManagerAdapter.hpp"
 
-#include "Teuchos_StandardMemberCompositionMacros.hpp"
-
-#include "Thyra_LinearOpWithSolveBase.hpp"
-#include "Thyra_LinearOpSourceBase.hpp"
+#include <Thyra_LinearOpWithSolveBase.hpp>
+#include <Thyra_LinearOpSourceBase.hpp>
 
 namespace Thyra
 {
@@ -59,7 +59,135 @@ namespace Thyra
 template<class Scalar>
 class MCLSLinearOpWithSolve : virtual public LinearOpWithSolveBase<Scalar>
 {
+  public:
 
+    //@{
+    //! Typedefs.
+    typedef MultiVectorBase<Scalar>                  MultiVector;
+    typedef LinearOpBase<Scalar>                     LinearOp;
+    //@}
+
+    // Uninitialized constructor.
+    MCLSLinearOpWithSolve();
+
+    /** \brief Initializes given precreated solver objects. */
+    void initialize(
+	const RCP<MCLS::LinearProblemAdapter<Scalar,MV_t,LO_t> >& linear_problem,
+	const RCP<Teuchos::ParameterList>& plist,
+	const RCP<MCLS::SolverManagerAdapter<Scalar,MV_t,LO_t> >&solver,
+	const RCP<const LinearOpSourceBase<Scalar> >& fwd_op_src,
+	const RCP<const PreconditionerBase<Scalar> >& prec,
+	const bool is_external_prec,
+	const RCP<const LinearOpSourceBase<Scalar> >& approx_fwd_op_src,
+	const ESupportSolveUse& support_solve_use,
+	const int conv_check_freq );
+  
+
+    /** @name Extraction methods */
+    //@{
+    /** \brief Extract the forward <tt>LinearOpBase<double></tt> object so that
+     * it can be modified.
+     */
+    RCP<const LinearOpSourceBase<double> > extract_fwdOpSrc();
+
+    /** \brief Extract the preconditioner.
+     */
+    RCP<const PreconditionerBase<double> > extract_prec();
+
+    /** \brief Determine if the preconditioner was external or not.
+     */
+    bool isExternalPrec() const;
+
+    /** \brief Extract the approximate forward <tt>LinearOpBase<double></tt>
+     * object used to build the preconditioner.
+     */
+    RCP<const LinearOpSourceBase<double> > extract_approxFwdOpSrc();
+    //@}
+
+
+    /** @name Overridden from LinearOpBase */
+    //@{
+    /** \brief. */
+    RCP<const VectorSpaceBase<double> > range() const;
+
+    /** \brief. */
+    RCP<const VectorSpaceBase<double> > domain() const;
+
+    /** \brief. */
+    RCP<const LinearOpBase<double> > clone() const;
+    //@}
+
+  protected: 
+
+    /** @name Overridden from LinearOpBase  */
+    //@{
+    /** \brief . */
+    virtual bool opSupportedImpl( EOpTransp M_trans ) const;
+
+    /** \brief . */
+    virtual void applyImpl( const EOpTransp M_trans,
+			    const MultiVectorBase<double> &X,
+			    const Ptr<MultiVectorBase<double> > &Y,
+			    const double alpha,
+			    const double beta ) const;
+    //@}
+
+
+    /** @name Overridden from LinearOpWithSolveBase. */
+    //@{
+    /** \brief . */
+    virtual bool solveSupportsImpl( EOpTransp M_trans ) const;
+
+    /** \brief . */
+    virtual bool solveSupportsNewImpl(
+	EOpTransp transp,
+	const Ptr<const SolveCriteria<Scalar> > solveCriteria ) const;
+
+    /** \brief . */
+    virtual bool solveSupportsSolveMeasureTypeImpl(
+	EOpTransp M_trans, const SolveMeasureType& solveMeasureType ) const;
+
+    /** \brief . */
+    virtual SolveStatus<Scalar> solveImpl(
+	const EOpTransp transp,
+	const MultiVectorBase<Scalar> &B,
+	const Ptr<MultiVectorBase<Scalar> > &X,
+	const Ptr<const SolveCriteria<Scalar> > solveCriteria ) const;
+    //@}
+
+  private:
+
+    // Check the initialization.
+    void assertInitialized() const;
+
+  private:
+
+    // Blocked linear problem.
+    RCP<MCLS::LinearProblemAdapter<Scalar,MV_t,LO_t> > d_linear_problem;
+
+    // Solver parameters.
+    RCP<Teuchos::ParameterList> d_plist;
+
+    // Blocked solver manager.
+    RCP<MCLS::SolverManagerAdapter<Scalar,MV_t,LO_t> > d_solver;
+
+    // Check frequency for convergence.
+    int d_conv_check_freq;
+
+    // Linear operator source.
+    RCP<const LinearOpSourceBase<Scalar> > d_fwd_op_src;
+
+    // Preconditioner.
+    RCP<const PreconditionerBase<Scalar> > d_prec;
+
+    // External preconditioner status.
+    bool d_is_external_prec;
+
+    // Support status.
+    ESupportSolveUse d_support_solve_use;
+
+    // Default convergence tolerance.
+    typename Teuchos::ScalarTraits<Scalar>::magnitudeType d_default_tol;
 };
 
 //---------------------------------------------------------------------------//
