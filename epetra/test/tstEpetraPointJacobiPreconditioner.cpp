a645980743458d18cbfe430990d25bde895684a0
Stuart Slattery <sslattery617@gmail.com>
2013-03-09 15:40:13 -0600

    adding point jacobi preconditioning test for epetra

---
+++ epetra/test/tstEpetraPointJacobiPreconditioner.cpp
@@ -0,0 +1,200 @@
+//---------------------------------------------------------------------------//
+/*
+  Copyright (c) 2012, Stuart R. Slattery
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+
+  *: Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+
+  *: Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
+
+  *: Neither the name of the University of Wisconsin - Madison nor the
+  names of its contributors may be used to endorse or promote products
+  derived from this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+//---------------------------------------------------------------------------//
+/*!
+ * \file tstTpetraPointJacobiPreconditioner.cpp
+ * \author Stuart R. Slattery
+ * \brief Tpetra::CrsMatrix adapter tests.
+ */
+//---------------------------------------------------------------------------//
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <sstream>
+#include <algorithm>
+#include <string>
+#include <cassert>
+
+#include <MCLS_MatrixTraits.hpp>
+#include <MCLS_VectorTraits.hpp>
+#include <MCLS_TpetraAdapter.hpp>
+#include <MCLS_Preconditioner.hpp>
+#include <MCLS_TpetraPointJacobiPreconditioner.hpp>
+
+#include <Teuchos_UnitTestHarness.hpp>
+#include <Teuchos_DefaultComm.hpp>
+#include <Teuchos_CommHelpers.hpp>
+#include <Teuchos_RCP.hpp>
+#include <Teuchos_ArrayRCP.hpp>
+#include <Teuchos_Array.hpp>
+#include <Teuchos_ArrayView.hpp>
+#include <Teuchos_TypeTraits.hpp>
+
+#include <Tpetra_Map.hpp>
+#include <Tpetra_Vector.hpp>
+#include <Tpetra_CrsMatrix.hpp>
+
+//---------------------------------------------------------------------------//
+// Instantiation macro. 
+// 
+// These types are those enabled by Tpetra under explicit instantiation.
+//---------------------------------------------------------------------------//
+#define UNIT_TEST_INSTANTIATION( type, name )			           \
+    TEUCHOS_UNIT_TEST_TEMPLATE_3_INSTANT( type, name, int, int, double )   \
+    TEUCHOS_UNIT_TEST_TEMPLATE_3_INSTANT( type, name, int, long, double )
+
+//---------------------------------------------------------------------------//
+// Test templates
+//---------------------------------------------------------------------------//
+TEUCHOS_UNIT_TEST_TEMPLATE_3_DECL( TpetraPointJacobiPreconditioner, diag_matrix, LO, GO, Scalar )
+{
+    typedef Tpetra::CrsMatrix<Scalar,LO,GO> MatrixType;
+    typedef Tpetra::Vector<Scalar,LO,GO> VectorType;
+    typedef MCLS::VectorTraits<VectorType> VT;
+    typedef MCLS::MatrixTraits<VectorType,MatrixType> MT;
+    typedef typename MT::scalar_type scalar_type;
+    typedef typename MT::local_ordinal_type local_ordinal_type;
+    typedef typename MT::global_ordinal_type global_ordinal_type;
+
+    Teuchos::RCP<const Teuchos::Comm<int> > comm = 
+	Teuchos::DefaultComm<int>::getComm();
+    int comm_size = comm->getSize();
+
+    int local_num_rows = 10;
+    int global_num_rows = local_num_rows*comm_size;
+    Teuchos::RCP<const Tpetra::Map<LO,GO> > map = 
+	Tpetra::createUniformContigMap<LO,GO>( global_num_rows, comm );
+
+    Teuchos::RCP<MatrixType> A = Tpetra::createCrsMatrix<Scalar,LO,GO>( map );
+    Teuchos::Array<GO> global_columns( 1 );
+    Scalar diag_val = 2.0;
+    Teuchos::Array<Scalar> values( 1, diag_val );
+    for ( int i = 0; i < global_num_rows; ++i )
+    {
+	global_columns[0] = i;
+	A->insertGlobalValues( i, global_columns(), values() );
+    }
+    A->fillComplete();
+
+    // Build the preconditioner.
+    Teuchos::RCP<MCLS::Preconditioner<MatrixType> > preconditioner = 
+	Teuchos::rcp( new MCLS::TpetraPointJacobiPreconditioner<Scalar,LO,GO>() );
+    TEST_ASSERT( Teuchos::is_null(preconditioner->getValidParameters()) );
+    TEST_ASSERT( Teuchos::is_null(preconditioner->getCurrentParameters()) );
+    preconditioner->setOperator( A );
+    preconditioner->buildPreconditioner();
+    Teuchos::RCP<const MatrixType> M = preconditioner->getPreconditioner();
+
+    // Check the preconditioner.
+    Teuchos::RCP<VectorType> X = MT::cloneVectorFromMatrixRows(*A);
+    MT::getLocalDiagCopy( *M, *X );
+    Teuchos::ArrayRCP<const Scalar> X_view = VT::view( *X );
+    typename Teuchos::ArrayRCP<const Scalar>::const_iterator view_iterator;
+    for ( view_iterator = X_view.begin();
+	  view_iterator != X_view.end();
+	  ++view_iterator )
+    {
+	TEST_EQUALITY( *view_iterator, 1.0/diag_val*comm_size );
+    }
+}
+
+UNIT_TEST_INSTANTIATION( TpetraPointJacobiPreconditioner, diag_matrix )
+
+//---------------------------------------------------------------------------//
+TEUCHOS_UNIT_TEST_TEMPLATE_3_DECL( TpetraPointJacobiPreconditioner, tridiag_matrix, LO, GO, Scalar )
+{
+    typedef Tpetra::CrsMatrix<Scalar,LO,GO> MatrixType;
+    typedef Tpetra::Vector<Scalar,LO,GO> VectorType;
+    typedef MCLS::VectorTraits<VectorType> VT;
+    typedef MCLS::MatrixTraits<VectorType,MatrixType> MT;
+    typedef typename MT::scalar_type scalar_type;
+    typedef typename MT::local_ordinal_type local_ordinal_type;
+    typedef typename MT::global_ordinal_type global_ordinal_type;
+
+    Teuchos::RCP<const Teuchos::Comm<int> > comm = 
+	Teuchos::DefaultComm<int>::getComm();
+    int comm_size = comm->getSize();
+
+    int local_num_rows = 10;
+    int global_num_rows = local_num_rows*comm_size;
+    Teuchos::RCP<const Tpetra::Map<LO,GO> > map = 
+	Tpetra::createUniformContigMap<LO,GO>( global_num_rows, comm );
+
+    Teuchos::RCP<MatrixType> A = Tpetra::createCrsMatrix<Scalar,LO,GO>( map );
+    Teuchos::Array<GO> global_columns( 1 );
+    Scalar diag_val = 2.0;
+    Teuchos::Array<Scalar> values( 3, diag_val );
+    for ( int i = 1; i < global_num_rows-1; ++i )
+    {
+	global_columns[0] = i-1;
+	global_columns[1] = i;
+	global_columns[2] = i;
+	A->insertGlobalValues( i, global_columns(), values() );
+    }
+    A->insertGlobalValues( 0, Teuchos::Array<GO>(1,0)(), 
+			   Teuchos::Array<Scalar>(1,diag_val)() );
+    A->insertGlobalValues( global_num_rows-1, 
+			   Teuchos::Array<GO>(1,global_num_rows-1)(), 
+			   Teuchos::Array<Scalar>(1,diag_val)() );
+    A->fillComplete();
+
+    // Build the preconditioner.
+    Teuchos::RCP<MCLS::Preconditioner<MatrixType> > preconditioner = 
+	Teuchos::rcp( new MCLS::TpetraPointJacobiPreconditioner<Scalar,LO,GO>() );
+    TEST_ASSERT( Teuchos::is_null(preconditioner->getValidParameters()) );
+    TEST_ASSERT( Teuchos::is_null(preconditioner->getCurrentParameters()) );
+    preconditioner->setOperator( A );
+    preconditioner->buildPreconditioner();
+    Teuchos::RCP<const MatrixType> M = preconditioner->getPreconditioner();
+
+    // Check the preconditioner.
+    Teuchos::RCP<VectorType> X = MT::cloneVectorFromMatrixRows(*A);
+    MT::getLocalDiagCopy( *M, *X );
+    Teuchos::ArrayRCP<const Scalar> X_view = VT::view( *X );
+    typename Teuchos::ArrayRCP<const Scalar>::const_iterator view_iterator;
+    for ( view_iterator = X_view.begin();
+	  view_iterator != X_view.end();
+	  ++view_iterator )
+    {
+	TEST_EQUALITY( *view_iterator, 1.0/diag_val*comm_size );
+    }
+}
+
+UNIT_TEST_INSTANTIATION( TpetraPointJacobiPreconditioner, tridiag_matrix )
+
+//---------------------------------------------------------------------------//
+// end tstTpetraPointJacobiPreconditioner.cpp
+//---------------------------------------------------------------------------//
+
