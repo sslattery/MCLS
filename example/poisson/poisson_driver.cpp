90334c53877541af4fe56b311a6caaf7700c53f9
Stuart R. Slattery <sslattery617@gmail.com>
2014-01-23 10:25:43 -0500

    adding simple poisson problem example

---
+++ example/poisson/poisson_driver.cpp
@@ -0,0 +1,201 @@
+//---------------------------------------------------------------------------//
+/*!
+ * \file poisson_driver.cpp
+ * \author Stuart R. Slattery
+ * \brief 1D poisson driver.
+ */
+//---------------------------------------------------------------------------//
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <sstream>
+#include <algorithm>
+#include <string>
+#include <cassert>
+
+#include <MCLS_SolverFactory.hpp>
+#include <MCLS_SolverManager.hpp>
+#include <MCLS_LinearProblem.hpp>
+#include <MCLS_EpetraAdapter.hpp>
+
+#include <Teuchos_GlobalMPISession.hpp>
+#include <Teuchos_FancyOStream.hpp>
+#include <Teuchos_DefaultComm.hpp>
+#include <Teuchos_CommHelpers.hpp>
+#include <Teuchos_RCP.hpp>
+#include <Teuchos_ArrayRCP.hpp>
+#include <Teuchos_Array.hpp>
+#include <Teuchos_as.hpp>
+#include <Teuchos_OpaqueWrapper.hpp>
+#include <Teuchos_TypeTraits.hpp>
+#include <Teuchos_Tuple.hpp>
+#include <Teuchos_ParameterList.hpp>
+#include "Teuchos_CommandLineProcessor.hpp"
+#include "Teuchos_XMLParameterListCoreHelpers.hpp"
+#include <Teuchos_TimeMonitor.hpp>
+#include <Teuchos_Time.hpp>
+
+#include <Epetra_Map.h>
+#include <Epetra_Vector.h>
+#include <Epetra_RowMatrix.h>
+#include <Epetra_Comm.h>
+#include <Epetra_SerialComm.h>
+
+#ifdef HAVE_MPI
+#include <Epetra_MpiComm.h>
+#include <Teuchos_DefaultMpiComm.hpp>
+#endif
+
+//---------------------------------------------------------------------------//
+// Typedefs.
+//---------------------------------------------------------------------------//
+
+typedef Epetra_Vector Vector;
+typedef MCLS::VectorTraits<Vector> VT;
+typedef Epetra_RowMatrix Matrix;
+typedef MCLS::MatrixTraits<Vector,Matrix> MT;
+
+//---------------------------------------------------------------------------//
+// Helper functions.
+//---------------------------------------------------------------------------//
+Teuchos::RCP<Epetra_Comm> getEpetraComm( 
+    const Teuchos::RCP<const Teuchos::Comm<int> >& comm )
+{
+#ifdef HAVE_MPI
+    Teuchos::RCP< const Teuchos::MpiComm<int> > mpi_comm = 
+    	Teuchos::rcp_dynamic_cast< const Teuchos::MpiComm<int> >( comm );
+    Teuchos::RCP< const Teuchos::OpaqueWrapper<MPI_Comm> > opaque_comm = 
+    	mpi_comm->getRawMpiComm();
+    return Teuchos::rcp( new Epetra_MpiComm( (*opaque_comm)() ) );
+#else
+    return Teuchos::rcp( new Epetra_SerialComm() );
+#endif
+}
+
+//---------------------------------------------------------------------------//
+int main( int argc, char * argv[] )
+{
+    // Initialize parallel communication.
+    Teuchos::GlobalMPISession mpi_session( &argc, &argv );
+    Teuchos::RCP<const Teuchos::Comm<int> > comm =
+	Teuchos::DefaultComm<int>::getComm();
+    Teuchos::RCP<Epetra_Comm> epetra_comm = getEpetraComm( comm );
+
+    // Initialize output stream.
+    Teuchos::FancyOStream out( Teuchos::rcpFromRef( std::cout ) );
+    out.setOutputToRootOnly( 0 );
+    out.setShowProcRank( true );
+
+    // Read in command line options.
+    std::string xml_input_filename;
+    Teuchos::CommandLineProcessor clp(false);
+    clp.setOption( "xml-in-file",
+		   &xml_input_filename,
+		   "The XML file to read into a parameter list" );
+    clp.parse(argc,argv);
+
+    // Build the parameter list from the xml input.
+    Teuchos::RCP<Teuchos::ParameterList> plist =
+	Teuchos::rcp( new Teuchos::ParameterList() );
+    Teuchos::updateParametersFromXmlFile(
+	xml_input_filename, Teuchos::inoutArg(*plist) );
+
+    // Setup parallel distribution.
+    int comm_size = comm->getSize();
+    int local_num_rows = plist->get<int>("Problem Size");
+    int global_num_rows = local_num_rows*comm_size;
+    Teuchos::RCP<Epetra_Map> map = Teuchos::rcp(
+	new Epetra_Map( global_num_rows, 0, *epetra_comm ) );
+
+    // Build the linear system.
+    Teuchos::RCP<Epetra_CrsMatrix> A = 	
+	Teuchos::rcp( new Epetra_CrsMatrix( Copy, *map, 0 ) );
+    Teuchos::Array<int> global_columns( 3 );
+    Teuchos::Array<double> values( 3 );
+    global_columns[0] = 0;
+    global_columns[1] = 1;
+    values[0] = 1.0;
+    values[1] = -0.5;
+    A->InsertGlobalValues( 0, 2, &values[0], &global_columns[0] );
+    for ( int i = 1; i < global_num_rows-1; ++i )
+    {
+	global_columns[0] = i-1;
+	global_columns[1] = i;
+	global_columns[2] = i+1;
+	values[0] = -0.5;
+	values[1] = 1.0;
+	values[2] = -0.5;
+	A->InsertGlobalValues( i, 3, 
+			       &values[0], &global_columns[0] );
+    }
+    global_columns[0] = global_num_rows-2;
+    global_columns[1] = global_num_rows-1;
+    values[0] = -0.5;
+    values[1] = 1.0;
+    A->InsertGlobalValues( global_num_rows-1, 2, 
+			   &values[0], &global_columns[0] );
+    A->FillComplete();
+
+    // Build the LHS by filling it with the specified fourier mode.
+    int k = plist->get<int>("Wave Number");
+    Teuchos::RCP<Vector> x = MT::cloneVectorFromMatrixRows( *A );
+    double pi = std::atan(1.0)*4.0;
+    for ( int i = 0; i < local_num_rows; ++i )
+    {
+	(*x)[i] = std::sin( i*k*pi / local_num_rows ) + 0.0;
+    }
+
+    // Build the RHS. The problem is homogeneous.
+    Teuchos::RCP<Vector> b = MT::cloneVectorFromMatrixRows( *A );
+    VT::putScalar( *b, 0.0 );
+
+    // Create the linear problem.
+    Teuchos::RCP<MCLS::LinearProblem<Vector,Matrix> > linear_problem =
+	Teuchos::rcp( new MCLS::LinearProblem<Vector,Matrix>(
+			  A, x, b ) );
+
+    // Create the solver.
+    std::string solver_type = plist->get<std::string>("Solver Type");
+    MCLS::SolverFactory<Vector,Matrix> factory;
+    Teuchos::RCP<MCLS::SolverManager<Vector,Matrix> > solver_manager =
+	factory.create( solver_type, comm, plist );
+    solver_manager->setProblem( linear_problem );
+
+    // Solve the problem.
+    Teuchos::Time timer("");
+    timer.start(true);
+    solver_manager->solve();
+    timer.stop();
+    if ( comm->getRank() == 0 )
+    {
+	std::cout << "MCLS Solve: Complete in " << timer.totalElapsedTime() 
+		  << " seconds." << std::endl;
+    }
+
+    // The point-wise error is the 2-norm.
+    std::cout << "||e||_2 = " << VT::norm2( *x ) << std::endl;
+
+    // Write the solution to a file.
+    if ( comm->getRank() == 0 )
+    {
+        std::ofstream ofile;
+        ofile.open( "solution.dat" );
+        for ( int i = 0; i < local_num_rows; ++i )
+        {
+            ofile << std::setprecision(8) << (*x)[i] << std::endl;
+        }
+        ofile.close();
+    }
+    comm->barrier();
+
+
+    return 0;
+}
+
+//---------------------------------------------------------------------------//
+// end neutron_diffusion.cpp
+//---------------------------------------------------------------------------//
+
---
+++ example/poisson/poisson_example.plot
@@ -0,0 +1,8 @@
+# gnuplot
+set autoscale
+set xtic auto
+set ytic auto
+set title "Poisson Example"
+set xlabel "Grid Point"
+set ylabel "x"
+plot "solution.dat" with line
